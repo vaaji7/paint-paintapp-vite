d330ffd0c3c327f873af293786add3a3dfdb9a80
parani92 <paranidharan92@gmail.com>
2025-08-18 03:46:20 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/paint-paintapp-vite/', // 👈 repo name, must match exactly
+  base: '/paint-paintapp-vite/', // 👈 must match your repo name
 })
