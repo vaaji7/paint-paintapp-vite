77400e4ad43ca0eed02a43212e6f00549d097abc
parani92 <paranidharan92@gmail.com>
2025-08-18 03:27:05 +0530

    C

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/<REPO>/',        // IMPORTANT for GitHub Pages when it's a project page
   plugins: [react()],
 })
